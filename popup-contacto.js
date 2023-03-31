fc23637bac08369e0510f0fdda81e3db5cf6ed1d
KeVincAsTrO000 <95428892+KeVincAsTrO000@users.noreply.github.com>
2023-03-31 19:25:55 -0300

    Add files via upload

---
+++ popup-contacto.js
@@ -0,0 +1,14 @@
+function window_open1(url){
+    window.open("https://api.whatsapp.com/send?phone=541156159768&text=Gracias%20por%20comunicarse", "nombrePop-Up", "width=380,height=500, top=85,left=50");
+   }
+     document.getElementById("botonWhatsapp").onclick = function() {window_open1()};
+
+function window_open2(url){
+      window.open("https://www.linkedin.com/in/kevin-castro-872823233/", "nombrePop-Up", "width=380,height=500, top=85,left=50");
+     }
+       document.getElementById("botonLinkedin").onclick = function() {window_open2()};
+
+          
+
+
+     
